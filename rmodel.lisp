e88968ff41606fae05685f1a329817faec295afd
Federico.Ferlito <federico.ferlito25@gmail.com>
2017-03-03 18:07:51 +0100

    second order fixed

---
+++ rmodel.lisp
@@ -0,0 +1,580 @@
+#|
+
+    Model:    First-order theory of mind reinforcement learning
+
+    Authors:  Arvid Lindström (s2740761)
+              Federico Ferlito (s2936860)
+              Francesco Dal Canton (s2935120)
+
+    Info:     The comments made by us are the ones that use the
+              multi-line specification (i.e. like this comment box).
+
+|#
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;
+;;;;;;	REINFORCEMENT LEARNING ACT-R model in order to show the developmental transitions in reasoning about false beliefs of others.
+;;;;;;  These stand from a child's reasoning from his/her own point of view (zero-order) to taking into consideration
+;;;;;;  an other agent’s beliefs (first-order) and later to taking into consideration an other agent’s beliefs about
+;;;;;;  other agents’ beliefs (second-order).
+;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;
+;;;;;;	Instructions on using this model:
+;;;;;;	  1. Make sure you have ACT-R 6.0
+;;;;;;	  2. Call (fbt) if you run the model for 1 child doing the experiment 1 time
+;;;;;;    3. Call (do-fbt) if you run the model for 1 child doing the experiment 100 times
+;;;;;;    4. Call (do-fbt-n N) if you run the model for N children doing the experiment 100 times
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(defvar *response* nil) ;; global variable that stores model output
+(defvar *model* nil)
+(defvar *hold-responses* nil)  ;; global variable that stores model output
+
+
+
+;; do experiment 1 time
+(defun fbt (&optional who)
+
+(goal-focus goal)
+  (if (eq who 'human)
+      (setf *model* nil)
+         (setf *model* t)
+   )
+        (run 30 :real-time t)
+)
+
+
+;; do experiment 100 times
+(defun do-fbt ()
+	(dotimes (i 100)
+            (setf *response* nil)
+			(fbt)
+               (push *response* *hold-responses*)
+         )
+)
+
+;; do experiment nx100 times
+(defun do-fbt-n (n)
+	  (dotimes (i n)
+              (setf *hold-responses* nil)
+                        (reset)
+			(do-fbt)
+                        (write-to-file (concatenate 'string "dat-" (write-to-string i)) (nreverse *hold-responses*))
+;(setf *hold-response* nil)
+         )
+)
+
+;; write results to a file
+(defun write-to-file (name lst)
+	(with-open-file
+		(out
+			(ensure-directories-exist
+				(merge-pathnames
+					(make-pathname :name name :directory '(:relative "ACTR_model_output") :type "txt")
+					"~/")
+                        )
+			:direction :output :if-does-not-exist :create :if-exists :supersede
+                 )
+			(dolist (line lst)
+				(format out "~{~a~^~t~}~%" line))
+         )
+)
+
+
+
+(clear-all) ;; clear model settings
+
+(define-model FBT_2   ;name of the model
+
+;;Model Parameters:
+
+;; Below you see the related parameters for the model. You’re expected to assign values to them or keep them default or turn them on (t) or off (nil)
+;; you can change the parameters once you have a complete model at the end of the course.
+;; when you write your scientific report, you are expected to explain your decisions about the parameters.
+
+(sgp
+  #|
+  Here we commented out the parameters that we did not need for this task, while we specified those that we needed.
+  |#
+  :esc t ;  sub-symbolic level
+  :ol t  ;  optimised learning
+  :rt -20  ;  retrieval threshold
+  :ans nil ;  instantaneous noise
+  :egs 0;  utility noise
+  :ul t  ;  utility learning
+  :pas nil; permanent noise
+
+;; The parameters below for tracking the model’s trace. You can change them if you need to turn off the details etc.
+;; See reference manual of ACT-R for further details, which is in the “docs” folder of ACT-R’s main folder
+
+        :ult t ;turn on the trace for the utilities
+        :v  t   ; trace detail
+        :act low  ; activation trace parameter
+        :sact low ; save activation trace
+
+)
+
+;Chunk-types of the Model:
+
+;You will use the following chunk-type for the story facts. Story facts are the sentences from the story, i.e., see lines 141-146.
+;The slot "subject" is for the subjects (i.e., "maxi", "sally", "mother").
+;The slot "negation" is for negating the verbs (i.e., either nil or "not").
+;The slot "verb" is for the verbs (i.e., "put", "see").
+;The slot "object" is for the object (i.e., "chips").
+;The slot "location" is for the locations (i.e., "cupboard", "oven", "trashbin").
+;The slot "time" is for when the story is happened (i.e., 1,2,3, which represent time t0, t1, t2)(see below the temporal order chunk explanation, lines 141-147).
+;The slot "type" is holding the type of a verb (i.e., action, perception).
+;The slot "self-ref" refers to the chunks name itself. Each story fact chunk will refer to itself.
+;The slot "ref" refers to other story facts which occur at the same time. (see below the temporal order chunk explanation, i.e., lines 141-146).
+;The slot "level" is to hold the previously retrieved level of reasoning chunk in order to use it when it is necessary. (You might need this at some point in first-order and second-order reasoning)
+
+(chunk-type story subject negation verb object location time type self-ref ref level)
+
+;For the time sequences of the events. (You might need these but it is not necessary if you find other ways to model).
+
+(chunk-type time-fact t0 t1 t2 t3 t4)
+
+; The following chunk type is for the goals and their states.
+; The slot "type" is for the where question (action)
+; The slot "output"  is for holding the output if the model (i.e., cupboard, oven, trashbin)
+(chunk-type goal state type output)
+
+
+(add-dm
+
+;The story fact chunks
+#|
+Here we added the relevant chunks for specifying the slots of the story chunks
+|#
+(not isa chunk) (action isa chunk) (perception isa chunk) (subject isa chunk)
+(location isa chunk) (verb isa chunk) (object isa chunk) (negation isa chunk)
+(put isa chunk) (see isa chunk) (type isa chunk) (Maxi isa chunk) (Sally isa chunk)
+(chips isa chunk) (mother isa chunk) (oven isa chunk) (cupboard isa chunk)
+(trashbin isa chunk) (r0 isa chunk) (r1 isa chunk) (r2 isa chunk) (r3 isa chunk)
+(r4 isa chunk)
+
+#|
+Here we added the states that are specific to the goal chunk (i.e. the values for the state slot).
+|#
+(start isa chunk) (retrieve0 isa chunk) (retrieved0 isa chunk) (done isa chunk)
+(retrieve-zero-percept isa chunk) (retrieve-one-percept isa chunk)
+(retrieve1 isa chunk) (retrieved1 isa chunk)
+
+(retrieve2 isa chunk) (retrieved2 isa chunk) (retrieved-time isa chunk)
+(retrieve-final isa chunk) (retrieved-final isa chunk)
+
+;temporal order chunk. There are three seperate time points in the story:
+;at t0 Maxi put the chips into the cupboard.
+;at t1 Sally put the chips into the oven.
+;at t1 Maxi saw Sally.
+;at t1 Sally did not see Maxi.
+;at t2 The mother put the chips into the trashbin.
+(t0 ISA time-fact t0 1 t1 2 t2 3)
+
+#|
+Here we defined the five story fact chunks. Each chunk follows the specification of the story chunk-type.
+Each chunk corresponds to one of the five sentences of the story, in order to correctly represent the way
+a child might remember the story after he reads or hears it.
+|#
+(story0 ISA story subject Maxi negation nil verb put object chips location cupboard time 1 type action self-ref story0 ref nil)
+(story1 ISA story subject Sally negation nil verb put object chips location oven time 2 type action self-ref story1 ref nil)
+(story2 ISA story subject Maxi negation nil verb see object Sally location nil time 2 type perception self-ref story2 ref story1)
+(story3 ISA story subject Sally negation not verb see object Maxi location nil time 2 type perception self-ref story3 ref story2)
+(story4 ISA story subject mother negation nil verb put object chips location trashbin time 3 type action self-ref story4 ref nil)
+
+#|
+Below chunk assigns the goal at the beginning of the trial. The state is set to start so that the start-response production
+can fire, while the type is set to action because this reflects the question posed to this model (i.e. "Where are the chips?").
+This because a 'where' question implies knowledge of where someone put something, and 'put' itself is an action, not a perception.
+|#
+(goal isa goal state start type action)
+
+)
+
+; For the Assignment 2, you are expected to write production rules to apply zero-order reasoning and gives
+; the answer "trashbin" (as if the model reasons about the question "Where is the chips?").
+
+; The production rule that gives the answer should also have the following functions for the output of the model:
+; To put 0 (zero) as a strategy level representing the zero-order strategy to the variable response:  !safe-eval! (push 0 *response*)
+; To write the name of values of the reasoning chunks:
+; !safe-eval! (push (sdp (reasoning-zero reasoning-zero-0 reasoning-zero-0-1) :name :utility :u :at :reward) *response*)
+
+#|
+This production starts the reasoning process. After checking whether the goal chunk
+has state 'start', it issues a request to the retrieval module to retrieve the story
+chunk relevant for the action type specified in the goal chunk, which also needs to have
+happened at time 2 (so we know it's the last story fact).  The goal state is set to
+'retrieve' for the next production to start.
+|#
+(p start-zero
+   =goal>
+      ISA         goal
+      state       start
+      type        =type
+==>
+   +retrieval>
+      ISA         story
+      type        =type
+      time        3
+   =goal>
+      state       retrieve0
+)
+
+#|
+This production fires if the goal is in the 'retrieve' state, if the retrieval of the
+story chunk was successful (multiple slots are checked for content), and if the imaginal
+model is not busy in other tasks.
+If that is the case, the retrieved chunk is added to the imaginal module, while
+the goal state is set to 'respond'.
+|#
+(p retrieve-zero
+   =goal>
+      ISA         goal
+      state       retrieve0
+   =retrieval>
+      ISA         story
+      subject     =sub
+      negation    nil
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+   ?imaginal>
+      state       free
+==>
+   +imaginal>
+      ISA         story
+      subject     =sub
+      negation    nil
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+   =goal>
+      state      retrieved0
+)
+
+#|
+This production starts when the goal chunk has state 'response', and when the relevant
+story fact was successfully stored in the imaginal buffer (again, multiple slots are checked
+for this condition).
+If these conditions are met, the goal state is set to 'done' to signify
+the end of the process, the output slot is finally set to the location contained in the chunk
+that is in the imaginal buffer. Along with this, a few functions are included to print the output
+in a separate file.
+|#
+(p respond-zero
+   =goal>
+      ISA         goal
+      state       retrieved0
+  =imaginal>
+      ISA         story
+      subject     =sub
+      negation    nil
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+==>
+  =goal>
+      state       done
+      output      =loc
+  !output!        (=loc)
+  !safe-eval!     (push 0 *response*)
+  !safe-eval! (push (spp (respond-zero start-one start-second) :name :utility :u :at :reward) *response*)
+)
+
+#|
+The model will attempt to simply retrieve the last time that Maxi saw something
+in order to take on his perspective, which is necessary to answer the first-order theory of mind question.
+|#
+(p start-one
+  =goal>
+      ISA         goal
+      state       retrieved0
+  =imaginal>
+      ISA         story
+==>
+  +retrieval>
+      ISA         story
+      subject     Maxi
+      negation    nil
+      type        perception
+      verb        see
+  =goal>
+      state       retrieve-one-percept
+)
+
+#|
+After having retrieved the chunk relative to Maxi's last perception, the model will retrieve
+the action that happened at the same time as Maxi's perception. This action will be the one
+which contains the location of the chips.
+|#
+(p retrieve-one
+  =goal>
+      ISA         goal
+      state       retrieve-one-percept
+  =retrieval>
+      ISA         story
+      subject     Maxi
+      negation    nil
+      type        perception
+      time        =time
+      ref         =r
+==>
+  =goal>
+      state       retrieve1
+  +retrieval>
+      ISA         story
+      negation    nil
+      type        action
+      time        =time
+      self-ref    =r
+)
+
+#|
+After having retrieved the chunk which contains the action of Sally moving the chips
+(which Maxi saw), the model will store this information in the imaginal buffer in order
+for it to be used in the next production.
+|#
+(p store-one
+  =goal>
+      ISA         goal
+      state       retrieve1
+  =retrieval>
+      ISA         story
+      subject     =sub
+      negation    nil
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+  ?imaginal>
+      state       free
+==>
+  =goal>
+      state       retrieved1
+  +imaginal>
+      ISA         story
+      subject     =sub
+      negation    nil
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+)
+
+#|
+In this production the model will check whether it has a chunk in the imaginal module,
+and if so it will produce an output which contains the location of the chips in that chunk.
+|#
+(p respond-one
+  =goal>
+      ISA         goal
+      state       retrieved1
+  =imaginal>
+      ISA         story
+      subject     =sub
+      negation    nil
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+==>
+  =goal>
+      state       done
+      output      =loc
+  !output!        (=loc)
+  !safe-eval!     (push 1 *response*)
+  !safe-eval! (push (spp (respond-zero start-one start-second) :name :utility :u :at :reward) *response*)
+  )
+
+
+;; At this point, the model is reasoning from Maxi's point of view. It already knows that
+;; Maxi saw Sally moving the chips. Now it need to take the perpective of Sally,
+;; in order to check whether or not she saw Maxi when she moved the chips.
+
+(p start-second
+  =goal>
+      ISA         goal
+      state       retrieved1
+  =imaginal>
+      ISA         story
+      subject     =sub
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+==>
+  +retrieval>
+      ISA         story
+      subject     =sub
+      time        =time
+      type        perception
+  =goal>
+      state       retrieve2
+  )
+
+;; store it in the immaginal buffer
+
+ (p retrieve-second
+   =goal>
+       ISA        goal
+       state      retrieve2
+   =retrieval>
+       ISA         story
+       subject     =sub
+       verb        =verb
+       object      =obj
+       time        =time
+    ?imaginal>
+       state       free
+==>
+    +imaginal>
+        ISA         story
+        subject     =sub
+        verb        =verb
+        object      =obj
+        time        =time
+    =goal>
+       state       retrieved2
+)
+
+
+;; retrive time to access the previous action, since no chunk is referring to Maxi's action
+(p retrieve-time
+  =goal>
+      ISA        goal
+      state      retrieved2
+  =immaginal>
+      ISA         story
+      subject     =sub
+      negation    not
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+==>
+  =immaginal>
+  +retrieval>
+      ISA         time-fact
+  =goal>
+      state       retrieved-time
+)
+
+; Once the model has the time and the last perceived thing of Sally,
+(p retrieve-last-action
+  =goal>
+      ISA        goal
+      state      retrieved-time
+  =immaginal>
+      ISA         story
+      subject     =sub
+      verb        =verb
+      object      =obj
+      time        =time1
+   =retrieval>
+      ISA         time-fact
+      t0          =time0
+      t1          =time1
+      t2          =time2
+==>
+  +retrieval>
+      ISA         story
+      -subject     =sub
+      negation    nil
+      type        action
+      time        =time0
+   =goal>
+      ISA         goal
+      state       retrieve-final
+  )
+
+  (p store-final
+    =goal>
+        ISA         goal
+        state       retrieve-final
+    =retrieval>
+        ISA         story
+        subject     =sub
+        negation    nil
+        verb        =verb
+        object      =obj
+        location    =loc
+        time        =time
+        type        =type
+    ?imaginal>
+        state       free
+  ==>
+    +imaginal>
+        ISA         story
+        subject     =sub
+        negation    nil
+        verb        =verb
+        object      =obj
+        location    =loc
+        time        =time
+        type        =type
+    =goal>
+        state       retrieved-final
+)
+
+(p respond-second
+  =goal>
+      ISA         goal
+      state       retrieved-final
+  =imaginal>
+      ISA         story
+      subject     =sub
+      negation    nil
+      verb        =verb
+      object      =obj
+      location    =loc
+      time        =time
+      type        =type
+==>
+  =goal>
+      state       done
+      output      =loc
+  !output!        (=loc)
+  !safe-eval! (push 2 response)
+  !safe-eval! (push (spp (respond-zero start-one start-second) :name :utility :u :at :reward) *response*)
+)
+
+
+
+;; The assignment will be graded in terms of the following criteria:
+;; 1) Output
+;; 2) Cognitive Plausibility of the production rules
+;; 3) The quality of the code document in terms of clear explanations.
+
+; For the Assignment 2, you're expected to write an initial utility value for the zero-order strategy below.
+; In the following assignments, you will also add intial utility values for the first-order and second-order strategies.
+
+(spp respond-zero :u 50)
+(spp respond-zero :reward 0)
+
+#|
+Here we set the utility and reward values for the first-order reasoning. The utility value is lower than for the zero-order,
+because it needs to be executed after the latter. The reward is set to a small value so that the improvement in performance is gradual.
+|#
+(spp start-one :u 20)
+(spp respond-one :reward 0)
+
+; For the Assignment 2, you're expected to write a reward value for the zero-order stategy below.
+; In the following assignments, you will also add reward values for the first-order and second-order strategies.
+
+(spp start-second :u 10)
+(spp respond-second :reward 0)
+)
